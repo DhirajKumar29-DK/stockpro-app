6d40ad8083a7d05ccfb7e99019c568a14ec4c10e
Dhiraj Kumar <201886612+DhirajKumar29-DK@users.noreply.github.com>
2026-06-01 14:37:25 +0530

    Update API URL for production

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://stockpro-app-b1xz.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
